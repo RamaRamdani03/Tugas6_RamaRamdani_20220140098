a2ca0ae874457142858ac3806b82d518fec6dee8
RamaRamdani03 <ramaramdani449@gamil.com>
2024-04-08 15:43:29 +0700

    FirstCommit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini Video Bring Me To Life");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini Audio Bring Me To Life");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini Gambar Evanescene");
+});
